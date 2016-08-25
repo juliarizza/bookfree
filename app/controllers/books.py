9f101e4d1414637275bfd888a9d437a02dc1d2d9
dragonavelar <dragonavelar@gmail.com>
2016-08-25 09:18:59 -0300

    Simple view for shelves and books in a shelf

--- app/controllers/books.py
+++ app/controllers/books.py
@@ -1,14 +1,45 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user
 
 from app import app, db
 from app.models.forms import SearchBook, NewBookForm
 from app.models.tables import Book, UserBook
+from flask_sqlalchemy import SQLAlchemy
+
+def or_(gen):
+    t = next(gen)
+    for g in gen:
+        t = t | g
+    return t
+
+
+@app.route('/shelf/<int:id>')
+def my_shelves(id):
+    #Pegando a lista de generos pra filtrar
+    filters = request.args.getlist('genre')
+    print(filters)
+    if len(filters) == 0: # Nenhuma estante selecionada
+        #qry = UserBook.query.join(Book).filter(UserBook.owner_id==id).group_by(Book.gender)
+        qry = db.session.query( UserBook.owner_id, db.func.count(UserBook.id).label("count"), Book.gender.label('genre') ).join(Book).filter( UserBook.owner_id==id ).group_by(Book.gender)
+        print( qry )
+        my_shelves = qry.all()
+        print(my_shelves)
+        return render_template('books/shelf.html',shelf=False,my_shelves=my_shelves)
+    else: # Filtra pelas estantes
+        qry = db.session.query( UserBook.id.label("id"), UserBook.owner_id, Book.title.label("title"), Book.author.label('author') ).join(Book).filter( (UserBook.owner_id==id) & or_( ( Book.gender==genre for genre in filters ) ) )
+        print( qry )
+        my_books = qry.all()
+        print( my_books )
+        return render_template('books/shelf.html',shelf=True,my_books=my_books)
+    return "bla" 
 
 
 @app.route('/books/<int:id>')
 def my_books(id):
-    my_books = UserBook.query.filter_by(owner_id=id).all()
+    my_books = UserBook.query.filter(UserBook.owner_id==id).all()
+    #print(my_books)
+    #for b in my_books:
+    #    print(b)
     return render_template('books/my_books.html',
                            my_books=my_books)
 
